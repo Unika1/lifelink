4fb6fdbc55a317a2f68125e2b250b7ff98c26d91
unika1 <unikaprj@gmail.com>
2025-12-04 21:15:51 +0545

    Changes made

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:lifelink/screens/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState(){
+    super.initState();
+    Future.delayed(const Duration(seconds:2),(){
+    Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(builder:(_)=>const OnboardingScreen()),
+    );
+    }); 
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body:Center(
+            child:Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                    Image.asset("assets/images/LifeLink.png"),
+                    SizedBox(height: 16),
+                ],
+                
+            )
+        )
+    );
+  }
+}
