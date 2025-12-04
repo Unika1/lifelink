796cf0c9016ad4b97c7ed7e191ffeeaa77127c40
unika1 <unikaprj@gmail.com>
2025-12-04 20:27:09 +0545

    Onboarding screen first commit

---
+++ lib/models/onboard_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OnboardItem {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+
+  const OnboardItem({
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+  });
+}
---
+++ lib/widgets/my_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({
+    super.key,
+    required this.text, 
+    required this.onPressed, 
+    this.color=Colors.red, 
+    this.textColor = Colors.white, 
+    });
+    final String text;
+    final VoidCallback onPressed;
+    final Color color;
+    final Color textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style:ElevatedButton.styleFrom(
+        backgroundColor: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25)
+        )
+      ), child: Text(
+        text,
+        style:TextStyle(
+          color: textColor,
+          fontSize:18,
+          fontWeight: FontWeight.w600,
+        )
+      )
+      ),
+    );
+  }
+}
