86d6ddc7268d28c015d38ead965ab61cc514a9ee
unika1 <unikaprj@gmail.com>
2025-12-04 16:29:34 +0545

    splash screen initial commit

---
+++ lib/sreens/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState(){
+    super.initState();
+    /*Future.delayed(const Duration(seconds:2),(){
+    Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(builder:(_)=>const LoginScreen()),
+    );
+    }); */
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body:Center(
+            child:Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                    Image.asset("assets/images/LifeLink.png",height: 120,),
+                    SizedBox(height: 16),
+                    Text(
+                    'LifeLink',style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.w700,
+                    ),
+                    )
+                ],
+                
+            )
+        )
+    );
+  }
+}
