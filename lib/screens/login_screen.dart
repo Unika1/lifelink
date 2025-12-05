f5b2fa4bb450bc37411a48d6cfba012570bc3bfb
unika1 <unikaprj@gmail.com>
2025-12-05 09:38:20 +0545

    Navigation Added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lifelink/common/my_snackbar.dart';
+import 'package:lifelink/screens/register_screen.dart';
 import 'package:lifelink/widgets/my_button.dart';
 import 'package:lifelink/widgets/my_textformfield.dart';
 
@@ -43,96 +44,99 @@ void dispose(){
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 50),
-              const Text(
-                "Login",
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 30),
-
-              MyTextformfield(
-                labelText: "Email", 
-                hintText: "Enter your email", 
-                controller: emailController,
-              ),
-              const SizedBox(height: 20),
-
-              MyTextformfield(
-                labelText: "Password", 
-                hintText: "Enter your password", 
-                controller: passwordController,
-              ),
-              const SizedBox(height: 10),
-
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: (){}, 
-                  child: const Text(
-                    "Forgot Password?",
-                    style: TextStyle(color: Colors.redAccent),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 50),
+                const Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-
-              MyButton(
-                text: "Login", 
-                color: Colors.redAccent,
-                onPressed:_handleLogin,
-              ),
-              const SizedBox(height: 25),
-              Row(
-                children: const[
-                  Expanded(child: Divider()),
-                  Padding(padding: EdgeInsets.symmetric(horizontal: 8),
-                  child: Text("OR"),
-                  ),
-                  Expanded(child: Divider()),
-                ],
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: double.infinity,
-                child: OutlinedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.g_mobiledata, size: 30),
-                  label: const Text("Continue with Google"),
-                  style: OutlinedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 48),
-                  ),
+                const SizedBox(height: 30),
+          
+                MyTextformfield(
+                  labelText: "Email", 
+                  hintText: "Enter your email", 
+                  controller: emailController,
                 ),
-              ),
-              const Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Don't have an account? "),
-                  GestureDetector(
-                    onTap: () {
-                      // TODO: Navigate to RegisterScreen
-                    },
+                const SizedBox(height: 20),
+          
+                MyTextformfield(
+                  labelText: "Password", 
+                  hintText: "Enter your password", 
+                  controller: passwordController,
+                ),
+                const SizedBox(height: 10),
+          
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: (){}, 
                     child: const Text(
-                      "Register",
-                      style: TextStyle(
-                        color: Colors.redAccent,
-                        fontWeight: FontWeight.bold,
-                      ),
+                      "Forgot Password?",
+                      style: TextStyle(color: Colors.redAccent),
                     ),
                   ),
-                ],
-              ), 
-              const SizedBox(height:20),             
-            ],
-          )
+                ),
+                const SizedBox(height: 20),
+          
+                MyButton(
+                  text: "Login", 
+                  color: Colors.redAccent,
+                  onPressed:_handleLogin,
+                ),
+                const SizedBox(height: 25),
+                Row(
+                  children: const[
+                    Expanded(child: Divider()),
+                    Padding(padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: Text("OR"),
+                    ),
+                    Expanded(child: Divider()),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  child: OutlinedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.g_mobiledata, size: 30),
+                    label: const Text("Continue with Google"),
+                    style: OutlinedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 48),
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account? "),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (_)=> const RegisterScreen()),
+                        );
+                      },
+                      child: const Text(
+                        "Register",
+                        style: TextStyle(
+                          color: Colors.redAccent,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ), 
+                const SizedBox(height:20),             
+              ],
+            )
+          ),
         ),
       ),
     );
