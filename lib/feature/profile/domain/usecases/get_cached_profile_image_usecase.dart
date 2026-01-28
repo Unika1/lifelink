5d13bcb73b35ca3818cc0115e8de2ea4c2f0f57a
unika1 <unikaprj@gmail.com>
2026-01-28 23:10:33 +0545

    usecases created

---
+++ lib/feature/profile/domain/usecases/get_cached_profile_image_usecase.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lifelink/core/error/failures.dart';
+import 'package:lifelink/core/usecases/app_usecase.dart';
+import 'package:lifelink/feature/profile/data/repository/profile_repository.dart';
+import 'package:lifelink/feature/profile/domain/repositories/profile_repository.dart';
+
+final getCachedProfileImageUsecaseProvider =
+    Provider<GetCachedProfileImageUsecase>((ref) {
+  final repo = ref.read(profileRepositoryProvider);
+  return GetCachedProfileImageUsecase(repo);
+});
+
+class GetCachedProfileImageUsecase implements UsecaseWithoutParams<String?> {
+  final IProfileRepository _repo;
+  GetCachedProfileImageUsecase(this._repo);
+
+  @override
+  Future<Either<Failure, String?>> call() {
+    return _repo.getCachedProfileImage();
+  }
+}
---
+++ lib/feature/profile/domain/usecases/upload_profile_usecase.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lifelink/core/error/failures.dart';
+import 'package:lifelink/core/usecases/app_usecase.dart';
+import 'package:lifelink/feature/profile/data/repository/profile_repository.dart';
+import 'package:lifelink/feature/profile/domain/repositories/profile_repository.dart';
+
+final uploadProfileImageUsecaseProvider =
+    Provider<UploadProfileImageUsecase>((ref) {
+  final repo = ref.read(profileRepositoryProvider);
+  return UploadProfileImageUsecase(repo);
+});
+
+class UploadProfileImageUsecase implements UsecaseWithParams<String, File> {
+  final IProfileRepository _repo;
+  UploadProfileImageUsecase(this._repo);
+
+  @override
+  Future<Either<Failure, String>> call(File params) {
+    return _repo.uploadProfileImage(params);
+  }
+}
