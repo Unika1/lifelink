e9f2f61e83ad6fd9f0d0411fe037e66b99300753
unika1 <unikaprj@gmail.com>
2026-02-16 01:06:57 +0545

    home entities created

---
+++ lib/feature/home/domain/entities/home_action_entity.dart
@@ -0,0 +1,11 @@
+class HomeActionEntity {
+  final String title;
+  final String description;
+  final String routeKey;
+
+  const HomeActionEntity({
+    required this.title,
+    required this.description,
+    required this.routeKey,
+  });
+}
